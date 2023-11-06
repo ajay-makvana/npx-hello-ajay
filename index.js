e94fadddb7df8e0d308a4d2521901e65a2a48bb9
ajay-makvana <ajaymakvana2001@gmail.com>
2023-11-06 15:40:56 +0530

    func: CLI added to print profile

---
+++ index.js
@@ -0,0 +1,20 @@
+const BOLD = "\x1b[1m";
+const RESET = "\x1b[0m";
+const GREEN = "\x1b[32m";
+const BLUE = "\x1b[36m";
+const YELLOW = "\x1b[33m";
+const UNDERSCORE = "\x1b[4m";
+
+console.log(`\n`);
+
+console.log(`${BOLD}${GREEN} INFO ${RESET} Hi I'm Ajay, a CSE Undergraduate from India`);
+
+console.log(`${BOLD}${GREEN} INFO ${RESET} I am writing ${YELLOW}Javascript ${RESET}and ${BLUE}React.js ${RESET}these days.`);
+
+console.log(`${BOLD}${GREEN} INFO ${RESET}${BOLD} Linkedin: ${RESET}${UNDERSCORE}https://www.linkedin.com/in/ajay-makvana ${RESET}`);
+
+console.log(`${BOLD}${GREEN} INFO ${RESET}${BOLD} Github: ${RESET}${UNDERSCORE}https://github.com/ajay-makvana ${RESET}`);
+
+console.log(`${BOLD}${YELLOW} WARN ${RESET} Connect with me on LinkedIn for more, See you ! ${RESET}`);
+
+console.log(`\n`);
